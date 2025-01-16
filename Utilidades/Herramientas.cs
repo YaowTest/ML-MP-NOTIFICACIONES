cc6405300a36b0440f81933e04e38536b305cb2a
123testcam <josecasillas202409@gmail.com>
2025-01-16 12:26:52 -0600

    Add files via upload

---
+++ Utilidades/Herramientas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Utilidades
+{
+    public class Herramientas
+    {
+        public void LogMPERP(string mensaje1)
+        {
+            //string logs = System.Web.HttpContext.Current.Server.MapPath("~/Recursos/Logs");
+            try
+            {
+                string logs = System.Web.HttpContext.Current.Server.MapPath("~/Recursos/Logs");
+                if (!Directory.Exists(logs))
+                    Directory.CreateDirectory(logs);
+
+                if (!File.Exists(logs + "\\logMPERP" + DateTime.Now.ToString("dd-MM-yyyy") + ".txt"))
+                {
+                    using (FileStream fs = File.Create(logs + "\\logMPERP" + DateTime.Now.ToString("dd-MM-yyyy") + ".txt"))
+                    {
+                    }
+                }
+                //if (!File.Exists(logs + "\\logMPERP" + DateTime.Now.ToString("dd-MM-yyyy") + ".txt"))
+                //File.Create(logs + "\\logMPERP" + DateTime.Now.ToString("dd-MM-yyyy") + ".txt");
+                File.AppendAllText(logs + @"\logMPERP" + DateTime.Now.ToString("dd-MM-yyyy") + ".txt", DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") + " " + mensaje1 + Environment.NewLine);
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
